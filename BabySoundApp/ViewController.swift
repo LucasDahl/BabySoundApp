84c5cfb116203867e38918a743f92fd81996b4c9
Lucas Dahl <LucasDahl425@yahoo.com>
2019-08-10 15:42:02 -0700

    Initial Commit

---
+++ BabySoundApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BabySoundApp
+//
+//  Created by Lucas Dahl on 8/10/19.
+//  Copyright © 2019 Lucas Dahl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
