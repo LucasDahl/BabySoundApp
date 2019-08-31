8b4b3a2183966627a75ed099c940c13a818f05b7
Lucas Dahl <LucasDahl425@yahoo.com>
2019-08-31 15:42:44 -0700

    Added the UI, hooked up timer labels and started setup for the timer.

--- BabySoundApp/ViewController.swift
+++ BabySoundApp/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Properties
+    var timer:Timer?
+    var seconds = 3600
+    
+    // IBOutlets
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var soundSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Create the timer
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerElapsed), userInfo: nil, repeats: true)
+        
+        // This allows the timer to work while scrolling
+        RunLoop.main.add(timer!, forMode: RunLoop.Mode.common)
+        
     }
+    
+    //================
+    // MARK: - Methods
+    //================
+    @objc func timerElapsed() {
+        
+        seconds -= 1
+        
+        // Set the label
+        timerLabel.text = "\(seconds)"
+        
+        // When the timer has reached 0
+        if seconds <= 0 {
 
+            // Stop the timer
+            timer?.invalidate()
+            timerLabel.textColor = UIColor.red
 
+        }
+    
+    }
+    
+    //==================
+    // MARK: - IBActions
+    //==================
+
+    @IBAction func timeButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
